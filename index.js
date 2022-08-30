635b6484126056a59499885b252ba391b22301e3
devanshi39 <devanshik.savla123@gmail.com>
2022-08-30 02:48:23 -0400

    added O/P file and modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require("cors");
 const path = require('path');
-
-
 const app = express();
 
 app.use(function (req, res, next) {
@@ -20,39 +18,37 @@ app.use(function (req, res, next) {
 	// Pass to next layer of middleware
 	next();
 });
-app.use((req,res,next) => {
-    console.log("Hello! from middleware ");
+app.use((req, res, next) => {
+	console.log("Hello! from middleware ");
 	next();
-    
+
 })
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 
 	let fs = require('fs');
-	// let filename = "test/input.csv";
 	let filename = "data/input.csv";
+	let opfile = "data/output.csv";
 	let lines = [];
-	fs.readFile(filename, 'utf8', function(err, data) {
+	fs.readFile(filename, 'utf8', function (err, data) {
 		if (err) throw err;
 		console.log('Read file - ' + filename);
 		lines = data.split(/\r?\n/);
-
-		for(let i=1;i<=5;i++) {
-			let val = lines[i-1].split(',');
-			if(val.length<2) continue;
+		let final_string = "";
+		for (let i = 0; i < lines.length; i++) {
+			let val = lines[i].split(',');
+			if (val.length < 2)
+				continue;
 			let a = val[0], b = val[1];
 			const c = +a + +b;
-			console.log("Result of operation " + i +" = " + c);
+			final_string += c + "\n";
 		}
-
-
+		fs.writeFile(opfile, final_string, function (err) {
+			if (err)
+				return console.error(err);
+			console.log("finished writing");
+		});
 	});
-
-	let a=20, b=89;
-    const c = a+b;
-    console.log("Result of operation = ",c);
-	
-	res.status(200).json({message : "Result of the operation", data:c})
-	
+	res.status(200).json({ message: "Output file created at location: /data/output.csv" });
 })
 module.exports = app;
