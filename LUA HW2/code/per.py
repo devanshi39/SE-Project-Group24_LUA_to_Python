f0895c0a88e4685a23f15be2e27f98724150965d
indranl1 <banerjeeindranil350@gmail.com>
2022-09-05 15:53:55 -0400

    Added per function

---
+++ LUA HW2/code/per.py
@@ -0,0 +1,17 @@
+# import modules 
+import csv, operator
+  
+# load csv file
+data = csv.reader(open('F:\Semester1\SE_Project\SE-Project-Group24\LUA\Input.csv'),delimiter=',')
+  
+# sort data on the basis of age
+data = sorted(data, key=)    
+  
+# displaying sorted data 
+print('After sorting:')
+print(data)
+
+
+
+
+def per(arr[],pos):    
