fdcf0e71106b51a6ba5122e938fcdc45dc1277fd
devanshi39 <devanshik.savla123@gmail.com>
2022-09-05 16:04:31 -0400

    sym method modified

--- LUA HW2/code/sym_methods.py
+++ LUA HW2/code/sym_methods.py
@@ -1,4 +1,5 @@
 #sym methods
+import math
 def add(int v):
 	if v!="?":
 		n=n+1
@@ -13,3 +14,11 @@ def mid(col, most, mode):
 	return mode
 
 def div(e, fun):
+	def fun(p):
+		return p*(math.log(p,2))
+	e=0
+	for n in self._has:
+		if n>0:
+			e=e-fun(n//self.n)
+	return e
+		
