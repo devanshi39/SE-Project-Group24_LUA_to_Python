d3acf3d0ae3225379d4a3a724d927f724d570c28
indranl1 <banerjeeindranil350@gmail.com>
2022-09-13 00:35:57 -0400

    modified row class

--- LUA HW2/code/row_class.py
+++ LUA HW2/code/row_class.py
@@ -2,3 +2,12 @@ import math
 import re
 from num_class import Num
 from sym_class import Sym
+
+class Row:
+    def __init__(self, t):
+        cells = t
+        cooked = copy (t)
+        isevaled = False
+        
+        `
+    
