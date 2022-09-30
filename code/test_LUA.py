61f296ca70db2048e51e34675e960221804e4ae4
devanshi39 <devanshik.savla123@gmail.com>
2022-09-30 02:12:13 -0400

    code coverage changes

---
+++ code/test_LUA.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    a=int(x)
+    b=int(y)
+    return (a+ b)
+
+def multiply(a, b):
+    y=int(b)
+    x=int(a)
+    print("here")
+    return (b+a)
+
+def test_add():
+    assert add(3,4) == 7
--- code/test_add.py
+++
@@ -1,6 +0,0 @@
-def add(x,y):
-    return (x + y)
-
-
-def test_add():
-    assert add(3,4) == 7
